6aa377a3319c1dda9ecf14d06a9f06fbc6ff4521
Gabriel Borgui <gabriel_borgui@hotmail.com>
2018-08-01 17:19:43 -0300

    terminar exercicio

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+    aqueleTime1: '',
+    aqueleTime2: '',
       gols1: 0,
       gols2: 0
     }
   }
   
+  time(event) {
+    this.setState({time: event.target.value})
+  }
+  time2(event) {
+    this.setState({time2: event.target.value})
+  }
+
+    
+
   incrementar = numero => {
     let novoState = {};
     novoState[`gols${numero}`] = this.state[`gols${numero}`] + 1;
@@ -22,20 +33,21 @@ class App extends Component {
       gols1: 0,
       gols2: 0
     });
+    
   }
   
   render() {
     return (
       <div>
         <div className="controles">
-          <input type="text" placeholder="Digite o nome do time da casa" />
-          <input type="text" placeholder="Digite o nome do time visitante" />
+          <input type="text" name ="time" value = {this.state.time} onChange={this.time.bind(this)} placeholder="Digite o nome do time da casa" />
+          <input type="text" name ="time2" value = {this.state.time2} onChange={this.time2.bind(this)}placeholder="Digite o nome do time visitante" />
           <button onClick={this.atualizar}>Zerar placar</button>
         </div>
         <div className="placar">
-          <PlacarTime numero="1" gols={this.state.gols1} nomeTime="Corinthians" funcaoBotao={this.incrementar} />
+          <PlacarTime numero="1" gols={this.state.gols1} nomeTime={this.state.time||"Time da casa"} funcaoBotao={this.incrementar} />
           X
-          <PlacarTime numero="2" gols={this.state.gols2}nomeTime="Chapecoense" funcaoBotao={this.incrementar} visitante="true"/>
+          <PlacarTime numero="2" gols={this.state.gols2} nomeTime={this.state.time2||"Time fora da casa"} funcaoBotao={this.incrementar} visitante="true"/>
         </div>
       </div>
     );
@@ -43,3 +55,4 @@ class App extends Component {
 }
 
 export default App;
+
